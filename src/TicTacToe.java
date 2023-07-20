d61fe26f7ceba28eb211e1b9d3ccc8687804a60e
PythonCoder55 <edwardbarchv@gmail.com>
2023-07-20 17:48:21 -0400

    added comments

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -17,23 +17,27 @@
 * If no, exit
 * */
 
+// Import scanner
 import java.util.Scanner;
 
 public class TicTacToe {
-
     private static final int ROW = 3;
     private static final int COL = 3;
     private static String[][] board = new String[ROW][COL];
     private static String currentPlayer = "X";
 
     public static void main(String[] args) {
+        // Clear board and initialize playAgain variable
         clearBoard();
         boolean playAgain;
 
         do {
+            // Display board and play game
             display();
             playGame();
+            // Ask if they want to play again
             playAgain = SafeInput.getYNConfirm(new Scanner(System.in), "Play again? (Y/N) or (y/n)");
+            // If yes, clear board and switch player to X
             if (playAgain) {
                 clearBoard();
                 currentPlayer = "X";
@@ -41,6 +45,7 @@ public class TicTacToe {
         } while (playAgain);
     }
 
+    // Clear the board by setting all cells to " "
     private static void clearBoard() {
         for (int i = 0; i < ROW; i++) {
             for (int j = 0; j < COL; j++) {
@@ -49,6 +54,7 @@ public class TicTacToe {
         }
     }
 
+    // Display the Tic-Tac-Toe board
     private static void display() {
         System.out.println("-------------");
         for (int i = 0; i < ROW; i++) {
@@ -60,14 +66,17 @@ public class TicTacToe {
         }
     }
 
+    // Check if a move is valid (within the board and the cell is not already taken
     private static boolean isValidMove(int row, int col) {
         return (row >= 0 && row < ROW && col >= 0 && col < COL && board[row][col].equals(" "));
     }
 
+    // Check if the specified player has won the game
     private static boolean isWin(String player) {
         return (isRowWin(player) || isColWin(player) || isDiagonalWin(player));
     }
 
+    // Check if the specified player has won in any row
     private static boolean isRowWin(String player) {
         for (int i = 0; i < ROW; i++) {
             if (board[i][0].equals(player) && board[i][1].equals(player) && board[i][2].equals(player)) {
@@ -77,6 +86,7 @@ public class TicTacToe {
         return false;
     }
 
+    // Check if the specified player has won in any column
     private static boolean isColWin(String player) {
         for (int j = 0; j < COL; j++) {
             if (board[0][j].equals(player) && board[1][j].equals(player) && board[2][j].equals(player)) {
@@ -86,6 +96,7 @@ public class TicTacToe {
         return false;
     }
 
+    // Check if the specified player has won in any diagonal
     private static boolean isDiagonalWin(String player) {
         if (((board[0][0].equals(player) && board[1][1].equals(player) && board[2][2].equals(player)) || (board[0][2].equals(player) && board[1][1].equals(player) && board[2][0].equals(player)))) {
             return true;
@@ -93,6 +104,7 @@ public class TicTacToe {
         return false;
     }
 
+    // Check if the game is a tie (all cells are filled)
     private static boolean isTie() {
         for (int i = 0; i < ROW; i++) {
             for (int j = 0; j < COL; j++) {
@@ -104,21 +116,27 @@ public class TicTacToe {
         return true;
     }
 
+    // Play the game
     private static void playGame() {
         Scanner in = new Scanner(System.in);
         int row, col;
 
         while (true) {
+            // Ask user for row and column
             row = SafeInput.getRangedInt(in, "Player " + currentPlayer + ", enter row (1-3)", 1, 3);
             col = SafeInput.getRangedInt(in, "Player " + currentPlayer + ", enter column (1-3)", 1, 3);
 
+            // Adjust row and column to match array indices
             row--;
             col--;
 
+            // Check if move is valid otherwise ask again
             if (isValidMove(row, col)) {
+                // Place move and display board
                 board[row][col] = currentPlayer;
                 display();
 
+                // Check if player wins or if it's a tie
                 if (isWin(currentPlayer)) {
                     System.out.println("Player " + currentPlayer + " wins!");
                     break;
@@ -127,6 +145,7 @@ public class TicTacToe {
                     break;
                 }
 
+                // Switch player
                 if (currentPlayer.equals("X")){
                     currentPlayer = "O";
                 }
